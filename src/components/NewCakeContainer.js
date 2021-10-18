478f4f531bdf207422245f7aa8ad6483f50f606c
Saidbek9092 <saidbek9092@gmail.com>
2021-10-18 16:05:40 +0500

    modified newcasecontainer

--- src/components/NewCakeContainer.js
+++ src/components/NewCakeContainer.js
@@ -9,6 +9,7 @@ const NewCakeContainer = (props) => {
             <h2>Number of cakes = {props.numberofCakes}</h2>
             <input type="text" value={number} onChange={e => setNumber(e.target.value)}/>
             <button onClick={() => props.buyCake(number)}>Buy {number} Cakes</button>
+
         </div>
     )
 }
