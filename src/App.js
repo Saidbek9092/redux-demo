52815fae41ccaf8403118ee253cfd9c6550c20ce
Saidbek9092 <saidbek9092@gmail.com>
2021-10-19 10:37:59 +0500

    added new features

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
             <div className="App">
                 {/*<UserContainer/>*/}
                 <ItemContainer cake/>
-                {/*<IceCreamContainer/>*/}
+                <IceCreamContainer/>
                 {/*<CakeContainer/>*/}
                 {/*<ItemContainer coffee/>*/}
                 {/*<CoffeeContainer/>*/}
--- src/redux/icecream/icecreamActions.js
+++ src/redux/icecream/icecreamActions.js
@@ -1,7 +1,6 @@
 import {BUY_ICECREAM} from "./icecreamTypes";
 
 export const buyIceCream = () => {
-
     return {
         type: BUY_ICECREAM
     }
