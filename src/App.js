2ed1d4ef48ca35fb0d7b4cd0b14d90aa89238d69
Saidbek9092 <saidbek9092@gmail.com>
2022-02-02 16:02:14 +0500

    PRAC-2: CourseList component created

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import Nav from "./components/old/Nav";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Shop from "./components/old/shop";
 import About from "./components/old/about";
-import User from "./components/old/user";
-import CoffeeContainer from "./components/old/CoffeeContainer";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import CakeContainer from "./components/old/CakeContainer";
-import { Test } from "./components/old/Test";
-import Video from "./components/old/Video";
+
 
 const App = () => {
 
@@ -19,10 +13,6 @@ const App = () => {
                 <Nav/>
                     <Route path="/shop" exact component={ Shop }/>
                     <Route path="/about" component={ About }/>
-                    <Route path="/shop/:userId" component={ Video }/>
-                    <Route path="/shop/:userId" component={ CakeContainer }/>
-                    <Route path="/shop/:userId" component={ Test }/>
-
             </div>
         </BrowserRouter>
 
