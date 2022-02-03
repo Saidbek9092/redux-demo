7fe486594657ee383748a593f9350618267bb25a
Saidbek9092 <saidbek9092@gmail.com>
2022-02-03 16:28:38 +0500

    d

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
 import './App.css';
-import Nav from "./components/old/Nav";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Shop from "./components/old/shop";
-import About from "./components/old/about";
-import User from "./components/old/user";
-import CoffeeContainer from "./components/old/CoffeeContainer";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import CakeContainer from "./components/old/CakeContainer";
-import { Test } from "./components/old/Test";
-import Video from "./components/old/Video";
+import { Form } from "react-bootstrap";
 
 const App = () => {
-
-    const handleData = () => {
-    }
     return (
-        // <BrowserRouter>
-        //     <div className="App">
-        //         <Nav/>
-        //             <Route path="/" exact component={ Home }/>
-        //             <Route path="/shop" exact component={ Shop }/>
-        //             <Route path="/shop/:userId" component={ User }/>
-        //             <Route path="/about" component={ About }/>
-        //
-        //     </div>
-        // </BrowserRouter>
-        <Video data={ handleData }/>
-
-    );
+        <Form>
+            <Form.Label>Question 1</Form.Label>
+            <Form.Check
+                type='radio'
+                id='1'
+                label='Answer 1'
+                name='1'
+            />
+            <Form.Check
+                type='radio'
+                id='2'
+                label='Answer 2'
+                name='2'
+            />
+            <Form.Check
+                type='radio'
+                id='3'
+                label='Answer 2'
+                name='3'
+            />
+        </Form>
+    )
 }
-
 
 export default App;
