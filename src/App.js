2517636698e8535496d2b2cd8d4174012b6c8149
Saidbek9092 <saidbek9092@gmail.com>
2022-03-13 16:11:08 +0500

    AgeHandler created

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ const App = () => {
         <div>
             {/*<h1>Use CallBack Example</h1>*/}
             {/*<AgeButton clickHandler={ageHandler} age={age}/>*/}
-
             {/*<SalaryButton clickHandler={salaryHandler} salary={salary}/>*/}
             <ParentComponent/>
         </div>
