df1df69b8d6978604f2701796461ef88922f7b6a
Saidbek9092 <saidbek9092@gmail.com>
2022-05-22 13:44:34 +0500

    added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Landing from "./components/Landing";
-import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Landing from './components/Landing'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
-    return(
-        <div>
-            <Router>
-                <Switch>
-                    <Route path="/">
-                        <Landing/>
-                        <Header/>
-                    </Route>
-                </Switch>
-            </Router>
-        </div>)
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Landing />
+            <Header />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App
